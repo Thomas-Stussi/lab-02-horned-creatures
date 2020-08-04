084042545d46468e54c024cb3dbeeec999b17f1f
Thomas Stussi <tstussi06@gmail.com>
2020-08-04 15:57:38 -0700

    image filtering

--- src/App.js
+++ src/App.js
@@ -5,14 +5,36 @@ import ImageList from './ImageList.js'
 import { images } from './data.js';
 
 
+let keywords = images.map(image => {
+    const container = [];
+    container.push(image.keyword)
+    return container;
+})
+
 
 class App extends React.Component {
+  state = {
+    filter: 'narwhal'
+  }
+  handleKeyword = (e) => {
+    const keyword = e.target.value;
+
+    this.setState({ filter: keyword})
+  }
+
   render() {
 
+    const filteredImages = images.filter(image => image.keyword === this.state.filter)
+    
     return (
       <>
-      <Header />
-      <ImageList images={images} />
+        <Header />
+        <select onChange={this.handleKeyword} name="" id="">
+          {
+            keywords.map(keyword => <option key={keyword} value={keyword}>{keyword}</option>)
+          }
+        </select>
+        <ImageList images={filteredImages} />
       </>
       );
   }
--- src/ImageItem.js
+++ src/ImageItem.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class ImageItem extends Component {
     render() {
         return (
-            <li>
-                <img src={this.props.image.url} alt='' />
-            </li>
+            <>
+                <p className="title">{this.props.image.title}</p> 
+                <img src={this.props.image.url.includes('modernfarmer') ? 'http://www.placekitten.com/200/200' : this.props.image.url} alt='' />
+                <p>{this.props.image.description}</p>
+            </>
         )
     }
 }
